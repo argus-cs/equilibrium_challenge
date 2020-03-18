e9fa147ae8ee42d8e1c483d22f89413f22e8b201
Argus <argus-cs@noreply.github.com>
2020-03-18 07:56:01 -0300

    Update 2020_03_17_021738_create_staff_table.php

--- database/migrations/2020_03_17_021738_create_staff_table.php
+++ database/migrations/2020_03_17_021738_create_staff_table.php
@@ -17,9 +17,11 @@ class CreateStaffTable extends Migration
             $table->increments('id');
             $table->string('cpf', 11)->unique();
             $table->string('name');
+            $table->string('carteira', 12);
 
             $table->integer('sectors_id')->unsigned();
-            $table->foreign('sectors_id')->references('id')->on('sectors');
+            $table->foreign('sectors_id')->references('id')->on('sectors')
+              ->onDelete('cascade');
 
             $table->timestamps();
         });
