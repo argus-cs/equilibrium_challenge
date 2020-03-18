f6749b21ab7a5d10d28b12f88b66e7afea4710a6
Augusto Santos <8759877+argus-cs@users.noreply.github.com>
2020-03-18 10:55:32 -0300

    Update index.blade.php

--- resources/views/staffs/index.blade.php
+++ resources/views/staffs/index.blade.php
@@ -3,10 +3,17 @@
 @section('title', 'Listar funcionarios')
 
 @section('content')
+<div class="media text-muted pt-3">
+  <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+  <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+    <strong class="d-block text-gray-dark">@username</strong>
+    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+  </p>
+</div>
+<small class="d-block text-right mt-3">
+  <a href="#">All updates</a>
+</small>
   <div class="container">
-    <div class="jumbotron">
-      <h1>Funcionários</h1>
-    </div>
     @if($staffs->isNotEmpty())
       <table class="table table-borded">
         <thead>
