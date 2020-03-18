1b8bf018d53ee0fbb9616ac4ddda736964d3cc17
Argus <argus-cs@noreply.github.com>
2020-03-18 07:56:04 -0300

    Create 2020_03_17_234431_create_phones_table.php

---
+++ database/migrations/2020_03_17_234431_create_phones_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('number', 11);
+
+            $table->integer('staff_id')->unsigned();
+            $table->foreign('staff_id')->references('id')->on('staff')
+              ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
